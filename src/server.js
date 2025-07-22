ff162bc712f6fa2e493ead03e50ff13b84002f4c
Denis Zherdetskyi <144510034+dizzzyy0@users.noreply.github.com>
2025-07-22 18:57:30 +0300

    create docker container

--- src/server.js
+++ src/server.js
@@ -1,18 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const prisma = new PrismaClient();
+
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Swagger setup
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Todo List API',
+            version: '1.0.0',
+        },
+        
+        servers: [{ url: 'http://localhost:3000' }],
+    },
+    apis: ['./src/routes/*.js'],
+};
+
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
 
 app.get('/', (req, res) =>{
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
